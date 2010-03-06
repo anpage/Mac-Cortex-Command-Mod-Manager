8cb839e73e8094ec0d54940a4d441d2f21477cfe
Frigid <ss2man44@gmx.com>
2010-03-05 23:47:27 -0500

    Removed accidental binary build.

--- build/Release/CC Mod Manager.app/Contents/Resources/BWToolkitFrameworkWrapper.py
+++
@@ -1,7 +0,0 @@
-import objc
-from Foundation import NSBundle
-
-objc.loadBundle("BWToolkitFramework",
-                globals(),
-                bundle_path=objc.pathForFramework(
-                NSBundle.mainBundle().privateFrameworksPath() + u'/BWToolkitFramework.framework'))
